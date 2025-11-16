e9e0ab877709fe528df45bfd854e93cd4f63a34c
guadalupealesandro <guadalupealesandro@gmail.com>
2025-11-16 21:02:58 +0000

    zlightgbm funcionando

--- babycode/z_backup /main-baye.py
+++ babycode/z_backup /main-baye.py
@@ -74,7 +74,7 @@ def main():
     #    random_state=42)
 
 
-    cols_to_drop = ['mprestamos_personales', 'cprestamos_personales'] # 'visa_cadelantosefectivo' 'cdescubierto_preacordado'
+    cols_to_drop = ['mprestamos_personales', 'cprestamos_personales']  #'active_quarter', 'cprestamos_prendarios','mprestamos_prendarios', 'mpayroll_2', 'mpayroll_2', 'visa_cadelantosefectivo' ,'ctrx_quarter' 'cdescubierto_preacordado'
     df_f = drop_columns(df_f, cols_to_drop)
     logger.info(f"✓ Datos eliminando prestamos personales: {df_f.shape}")
     
@@ -84,7 +84,7 @@ def main():
     df_f = normalizar_ctrx_quarter(df_f)
     logger.info(f"✓ Datos luego de normalización ctrx_quarter: {df_f.shape}")
     
-    col = ['mpayroll', 'mpayroll_2']
+    col = ['mpayroll']
     df_f = generar_sobre_edad(df_f, col)
     logger.info(f"✓ Datos agregando quarter/edad: {df_f.shape}")
 
@@ -138,6 +138,77 @@ def main():
 
 
 
+
+
+
+
+
+
+
+
+    
+    
+    # 2 - optimización de hiperparámetros
+    #logger.info("=== INICIANDO OPTIMIZACIÓN DE HIPERPARAMETROS ===")
+    #study = optimizar(df_f, n_trials= 30, undersampling = 0.05, repeticiones = 1, ksemillerio = 7)  
+
+    # 3 - Evaluar modelo en test
+    #best_params = cargar_mejores_hiperparametros_completo(n_top = 1)
+    #cortes = [9000, 9500, 10000, 10500, 12000, 12500, 13000, 16000, 18000]
+    #resultados_test, resultados_df_fijo, resultados_df, y_pred_binary_mejor, y_test, y_pred_promedio_total = evaluar_en_test_semillerio(df_f, best_params, cortes, repeticiones = 1, ksemillerio = 100)
+
+    # Resumen de evaluación en test
+    #logger.info("=== RESUMEN DE EVALUACIÓN EN TEST ===")
+    #logger.info(f"Ganancia en test: {resultados_test['ganancia_test']:,.0f}")
+    #logger.info(f"Predicciones positivas: {resultados_test['predicciones_positivas']:,} ({resultados_test['porcentaje_positivas']:.2f}%)")
+
+    # Grafico de test
+    #logger.info("=== GRAFICO DE TEST ===")
+    #ruta_grafico_avanzado = crear_grafico_ganancia_avanzado(y_true=y_test, y_pred_proba=y_pred_prob)
+    #logger.info(f"Gráficos generados: {ruta_grafico_avanzado}")
+
+    #guardar_predicciones_finales(resultados_df_fijo)
+    #guardar_predicciones_finales(resultados_df)
+
+
+
+    #logger.info("=== GENERANDO TABLA DE DECISIÓN DE CORTE ===")
+
+    #cortes = [9000, 9500, 10000, 10500, 12000, 12500, 13000, 16000, 18000]
+
+    #df_resultados = simular_cortes_kaggle(
+    #y_pred_prob=y_pred_prob,
+    #y_test=y_test,
+    #cortes=cortes,
+    #ganancia_por_corte=ganancia_por_corte,
+    #random_state=42)
+
+    # Resume las ganancias promedio por corte
+    #df_resumen = resumen_cortes(df_resultados)
+    #print("\n=== RESULTADOS DE SIMULACIÓN DE CORTES ===")
+    #print(df_resumen.to_string(index=False))
+
+    
+    # 7 Entrenar modelo final
+    #logger.info("=== ENTRENAMIENTO FINAL ===")
+    #logger.info("Preparar datos para entrenamiento final")
+    #X_train, y_train, X_predict, clientes_predict = preparar_datos_entrenamiento_final(df_f)
+
+    # Entrenar modelo final
+    #logger.info("Entrenar modelo final")
+    #modelo_final = entrenar_modelo_final(X_train, y_train, params_best_model)
+
+    # Generar predicciones finales
+    #logger.info("Generar predicciones finales")
+
+
+    #generar_predicciones_por_cantidad(modelo_final, X_predict, clientes_predict, cantidades = [9000, 9500, 10000, 10500, 12000, 12500, 13000, 16000, 18000])
+
+    # 4 Guardar el DataFrame resultante
+    #path = "Data/competencia_01_lag.csv"
+    #df.to_csv(path, index=False)
+    #logger.info(f"DataFrame resultante guardado en {path}")
+    
     logger.info(f">>>> Ejecución finalizada <<<< Para más detalles, ver {nombre_log}")
 
 
